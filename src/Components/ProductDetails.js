8332f713f8fde81ad417fcceb5725454c1ab5c40
Rohan Kesarkar <rohan.kesarkar@techmojo.in>
2022-08-04 06:25:14 -0700

    third commit

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const ProductDetails = () => {
     const prodId = useParams()
@@ -16,11 +18,28 @@ const ProductDetails = () => {
     console.log("This is ", item)
 
     const currentProduct = item.find(record=>record.id === parseInt(prodId.id))
-console.log("tttttttttttt", currentProduct)
+
 
   return (
     <>
-   ProductDetails
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={currentProduct.image} />
+      <Card.Body>
+        <Card.Title>{currentProduct.title}</Card.Title>
+        <Card.Text>
+        {currentProduct.deccription}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
     </>
   )
 }
