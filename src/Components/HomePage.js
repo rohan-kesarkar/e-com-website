d2b9cb19fbf03154553060ffa0a8fd763d923475
Rohan Kesarkar <rohan.kesarkar@techmojo.in>
2022-08-04 05:09:08 -0700

    first commit

---
+++ src/Components/HomePage.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+    const [item, setItem] = useState([])
+    
+    const getProduct = async ()=>{
+        const responce = await fetch("https://fakestoreapi.com/products")
+        setItem(await responce.json())
+    }
+   useEffect(()=>{
+    getProduct()
+   },[])
+  return (
+    <>
+     <Navbar item={item} setItem={setItem}/>
+    <Row xs={1} md={3} className="g-4">
+      {item.map((elem) => (
+        <Col key={elem.id}>
+          <Card>
+            <Card.Img variant="top" src={elem.image} style={{height:"200px", width:"200px"}} />
+            <Card.Body>
+              <Card.Title style={{width:"300px"}}>{elem.title}</Card.Title>
+              <Card.Text>
+               Price: ${elem.price}
+              </Card.Text>
+              <Card.Text>
+               Rating: {elem.rating.rate}
+              </Card.Text>
+              <Button variant="light">More Details</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+ </>
+  )
+}
+export default HomePage
---
+++ src/Components/Navbar.js
@@ -0,0 +1,91 @@
+import { BsCartPlus } from "react-icons/bs";
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+
+const Navbar = (props) => {
+    
+    const searchProduct = (e)=>{
+        const data = props.item.filter((rec)=>rec.title.toLowerCase().startsWith(e.target.value.toLowerCase()))
+        props.setItem(data)
+    }
+
+const ascendingSort = ()=>{
+    props.setItem((item)=>{
+        const dataToSort = [...item]
+        dataToSort.sort((a,b)=>Number(a.price)-Number(b.price))
+        return dataToSort
+    })
+}
+const decendingSort =()=>{
+    props.setItem((item)=>{
+        const dataToSort = [...item]
+        dataToSort.sort((a,b)=>Number(b.price)-Number(a.price))
+        return dataToSort
+    })
+}
+
+const ascendingRating = ()=>{
+    props.setItem((item)=>{
+        const dataToSort = [...item]
+        dataToSort.sort((a,b)=> Number(a.rating.rate)-Number(b.rating.rate))
+        return dataToSort
+    })
+}
+
+const descendingRating = ()=>{
+    props.setItem((item)=>{
+        const dataToSort = [...item]
+        dataToSort.sort((a,b)=> Number(b.rating.rate)-Number(a.rating.rate))
+        return dataToSort
+    })
+}
+
+const selectProduct = (e)=>{
+    const data = props.item.filter((res)=>res.category === e.target.value)
+    props.setItem(data)
+
+}
+
+  return (
+    <>
+    <div>
+    <h1>Shopping Mart...<span style={{float : "right", paddingRight:"30px", paddingTop:"1px"}}><BsCartPlus /></span></h1>
+    
+    
+    </div>
+   
+    
+    <div>
+        <input type="text" style={{width:"400px"}} placeholder="search product here..... " onKeyUp={searchProduct} />
+    </div>
+    <div>
+     <h4> Sorting</h4>  
+     <span>Price :</span>{' '}
+     <button onClick={ascendingSort} >Low to high</button>{' '}
+     <button onClick={decendingSort}>High to low</button>{' '}
+     <span>Rating :</span>{' '}
+     <button onClick={descendingRating}>High to Low</button>{' '}
+     <button onClick={ascendingRating}>Low to High</button>{' '}
+   
+     {' '}<Form.Select size="sm" style={{width:"350px", height:"40px"}}  onChange={selectProduct}>
+   
+     <option>Select Category</option>
+        {
+            props.item.map((res)=>{
+                return(
+            <option key={res.id}>
+                {res.category}
+                </option>
+                )
+            
+            })
+        }
+        
+      </Form.Select>
+    </div>
+   
+   </>
+  )
+}
+export default Navbar
